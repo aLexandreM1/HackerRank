9138b5dca2f7442a3f5a3ec74fd004cb0694c084
Xandao <alee_ppl@hotmail.com>
2018-02-07 02:44:15 -0200

    Algorithms - Some Solved Algorithms

---
+++ BigSum/aVeryBigSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    private static long result;
+
+    static long aVeryBigSum(int n, long[] ar) 
+    {
+        for(int i = 0; i < n; i++)
+        {
+            result = ar.Sum();
+        }
+            return ar.Sum();
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] ar_temp = Console.ReadLine().Split(' ');
+        long[] ar = Array.ConvertAll(ar_temp,Int64.Parse);
+        long result = aVeryBigSum(n, ar);
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
